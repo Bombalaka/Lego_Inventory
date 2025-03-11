18833afb24f7d2ae6b651a8b8b1c7cf795f0c1b1
Bombalaka <yotaka.johansson@hotmail.com>
2025-03-11 13:50:30 +0100

    Update MongoDB connection string and adjust Docker configuration for mongo-express service

--- obj/Debug/net9.0/Lego_Inventory.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lego_Inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7793c3466eb43b5f1ffb5c280ba6a2a4835024d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f03c95a02692b390224d01ed622a146ecf2365f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
