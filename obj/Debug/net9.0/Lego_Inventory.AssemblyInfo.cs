a7afab0dd3444dd79ae8625219a506e2899e105d
Bombalaka <yotaka.johansson@hotmail.com>
2025-03-17 21:48:31 +0100

    Update AssemblyInfo to reflect new version and regenerate cache

--- obj/Debug/net9.0/Lego_Inventory.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lego_Inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3066482f02b6a9c588539f45d3e05b3d3134eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372552204d271d78061ff11860ab3bbfbd42b80a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
