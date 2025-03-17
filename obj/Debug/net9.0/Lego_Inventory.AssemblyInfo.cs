b764743c301fe385ddbde168066d9981f7429dad
Bombalaka <yotaka.johansson@hotmail.com>
2025-03-17 18:03:44 +0100

    Add CosmosDB connection string to cloud-init configuration and update assembly version

--- obj/Debug/net9.0/Lego_Inventory.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lego_Inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5a1cd584923ad2eec7a6c17db3d83f231b4a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3066482f02b6a9c588539f45d3e05b3d3134eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lego_Inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
